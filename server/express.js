4322658ed5ebb659e0127c5d02e0b9d80ae0820f
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-12-17 13:26:45 +0100

    Add express.js

---
+++ server/express.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const compress = require('compression')
+const userRoute = require('./routes/users.route')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(compress())
+app.use(cookieParser())
+
+app.use('/api/users', userRoute)
+
+module.exports = app
