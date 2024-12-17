490998accdf257d60374980f0abc199360ac5333
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-12-17 14:10:16 +0100

    Feat: add uploads middleware

--- server/express.js
+++ server/express.js
@@ -12,6 +12,7 @@ app.use(cors())
 app.use(compress())
 app.use(cookieParser())
 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 app.use('/api/users', userRoute)
 
 module.exports = app
