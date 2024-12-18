38328a78d11cffd559c3eebb8fd847320d3e886e
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-12-18 23:43:53 +0100

    Feat: add requireAuth and enforce middleware

--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -1,12 +1,16 @@
 const userRoute = require('express').Router()
 const usersCntrl = require('../controller/users.controller')
+const enforce = require('../lib/enforce')
+const requireAuth = require('../lib/require-auth')
 
 userRoute.route('/')
     .get(usersCntrl.getUsersRoute)
+
+userRoute.use(requireAuth)
 userRoute.route('/:userId')
     .get(usersCntrl.getUserRoute)
-    .put(usersCntrl.updateUserRoute)
-    .delete(usersCntrl.deleteUserRoute)
+    .put(enforce(usersCntrl.userPolicy), usersCntrl.updateUserRoute)
+    .delete(enforce(usersCntrl.userPolicy), usersCntrl.deleteUserRoute)
 
 
 module.exports = userRoute
