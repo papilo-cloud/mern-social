462f1dfa673f305b93fa59bab038574fd7970acf
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-12-21 22:50:08 +0100

    Feat:  add methods for creating user and fetching sign-in and sign-out API endpoints

---
+++ client/src/utils/auth/api-auth.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const create = async (data) => {
+    try {
+        const response = await axios.post('/api/auth/register', data)
+        return response.data
+    } catch (err) {
+        return err.response.data
+    }
+}
+
+const signin = async (data) => {
+    try {
+        const response = await axios.post('/api/auth/signin', data, {
+            headers: {
+                Accept: 'application/json'
+            }
+        })
+        return response
+    } catch (err) {
+        return err.response
+    }
+    
+}
+
+const signout = async () => {
+    try {
+        const response = await axios.get('/api/auth/signout')
+        return response
+    } catch (err) {
+        return err.response
+    }
+}
+
+export {create, signin, signout}
