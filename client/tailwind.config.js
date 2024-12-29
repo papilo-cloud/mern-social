ebad44215428518aa329beb403bb6da7a419101f
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-12-29 23:42:37 +0100

    Add congig

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#121575'
+      }
+    },
   },
   plugins: [],
 }
