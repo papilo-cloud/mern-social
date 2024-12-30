c6bb529f3370ddc2eec66a6939057060e3b40a89
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-12-30 12:15:16 +0100

    Add Followgrid component

---
+++ client/src/components/core/FollowGrid/FollowGrid.tsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom'
+import Avatar from '../Avatar/Avatar'
+
+interface FollowGridProps {
+    array: {
+        _id: string,
+        name: string,
+        photo: string
+    }[]
+}
+const FollowGrid = ({array}: FollowGridProps) => {
+  return (
+    <div className='w-full relative flex justify-center items-center gap-10 mt-4'>
+        {
+            array?.map((arr, idx) => 
+                    <Link 
+                        className='flex flex-col items-center gap-2' 
+                        to={`/user/${arr._id}`}
+                        key={idx}>
+                        <Avatar photo={arr.photo} />
+                        <p>{arr.name}</p>
+                    </Link>
+             )
+        }
+    </div>
+  )
+}
+
+export default FollowGrid
