57eb71fc2684b9a5289717af440586cee0da01a6
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-12-30 12:18:31 +0100

    Add main route app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Layout } from "./Layout/Layout"
+import Signin from "./components/views/Signin"
+import Home from "./components/views/Home"
+import PrivateRoute from "./utils/auth/PrivateRoute"
+import EditProfile from "./components/views/EditProfile"
+import Profile from "./components/views/Profile"
+import Users from "./components/views/Users"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Layout />}>
+          <Route index element={ <Home />} />
+          <Route path="/signin" element={ <Signin />} />
+          <Route path="/user/:userId" element={ 
+              <PrivateRoute>
+                  <Profile />
+              </PrivateRoute>
+          } />
+          <Route path="/user/edit/:userId" element={ 
+              <PrivateRoute>
+                  <EditProfile />
+              </PrivateRoute>
+           } />
+          <Route path="/users" element={ <Users /> } />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
