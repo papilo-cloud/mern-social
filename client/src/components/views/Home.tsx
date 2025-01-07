bec03300fba253e5f8b5d8a3670ebab7784b206a
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-01-07 21:26:55 +0100

    Add component

--- client/src/components/views/Home.tsx
+++ client/src/components/views/Home.tsx
@@ -1,9 +1,21 @@
+import { auth } from '../../utils/auth/auth-helper'
+import NewsFeed from "./NewsFeed"
+import FindPeople from './FindPeople'
 
 const Home = () => {
+  const jwt = auth.isAuthenticated()
 
   return (
-    <div className=' justify-center items-center text-3xl flex flex-col mt-20 h:[100vh - 80px]'>
-        <h1>Hello world</h1>
+    <div className=' justify-center items-center text-3xl flex flex-col mt-20 h:[100vh - 80px] px-3'>
+        {
+          !jwt ? <h1>Hello world</h1>:
+          <div className='w-full flex gap-10'>
+              <NewsFeed />
+              <div className='w-[500px] h-full '>
+                  <FindPeople />
+              </div>
+          </div>
+        }
     </div>
   )
 }
