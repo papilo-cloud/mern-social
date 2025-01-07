e23a74dfe2a902b36fbd4651d0e0fd6ace72eecb
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-01-07 21:23:27 +0100

    Add snackbar component

---
+++ client/src/components/core/Snackbar/Snackbar.tsx
@@ -0,0 +1,33 @@
+import clsx from "clsx";
+import { ComponentProps, useEffect, useState } from "react";
+import { useAuth } from "../../../context/Context";
+
+export interface SnackbarProps extends ComponentProps<'div'> {
+    isOpen?: boolean,
+    timedOut: number,
+    textStyle?: string
+}
+
+const Snackbar = ({isOpen, textStyle, timedOut=3, className, children}: SnackbarProps) => {
+
+    const {auth} = useAuth()
+
+    useEffect(() => {
+        const onTimeout = () => {
+            auth.setOpen(false)
+        }
+        const timed = setTimeout(onTimeout , timedOut*1000);
+
+        return () => clearTimeout(timed)
+    }, [isOpen])
+
+  return (
+    <>{
+        isOpen && <div className={clsx('bg-black z-50 py-3 px-6 fixed top-4 left-[50%] translate-x-[-50%] animate-scale rounded-md', className)}>
+        <p className={clsx("text-red-700 text-base", textStyle)}>Following <b>{children}</b> !</p>
+    </div>
+    }</>
+  )
+}
+
+export default Snackbar
