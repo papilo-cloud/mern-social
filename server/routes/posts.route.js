f7a3a755abdc3fb52184ecb2b81f13486406b764
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-01-09 14:27:38 +0100

    Feat: add remove post route

--- server/routes/posts.route.js
+++ server/routes/posts.route.js
@@ -1,14 +1,18 @@
-const {listNewsFeed, listByUser, createPost} = require('../controller/posts.controller')
+const {listNewsFeed, listByUser, createPost,
+    postsPolicy, removePost} = require('../controller/posts.controller')
+const enforce = require('../lib/enforce')
 const requireAuth = require('../lib/require-auth')
 
 const postRoute = require('express').Router()
 
 postRoute.use(requireAuth)
+
 postRoute.route('/new/:userId')
     .post(createPost)
 postRoute.route('/feed/:userId')
     .get(listNewsFeed)
 postRoute.route('/by/:userId')
     .get(listByUser)
-
+postRoute.route('/:postId')
+    .delete(enforce(postsPolicy), removePost)
 module.exports = postRoute
