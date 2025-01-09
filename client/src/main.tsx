bed2ef20ef75af72b4fee3bc99d44546634a8139
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-01-09 14:25:22 +0100

    Feat: use context provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ContextProvider } from './context/Context.tsx'
+import { PostContextProvider } from './context/PostContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ContextProvider>
-      <App />
+      <PostContextProvider>
+        <App />
+      </PostContextProvider>
     </ContextProvider>
   </React.StrictMode>,
 )
